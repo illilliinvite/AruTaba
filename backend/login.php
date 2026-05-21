beb36bb64dbc0d3afa62e4372ba879eed40fcf0b
katokosei0216 <networkadoresu0216@gmail.com>
2026-05-21 09:53:24 +0900

    ログイン画面最新

---
+++ backend/login.php
@@ -0,0 +1,54 @@
+<?php
+
+//DB接続情報
+$host = "localhost";
+$dbname = "arutaba";
+$user = "root";
+$pass = "arutaba";
+
+try{
+
+    //mysql接続
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    //エラー表示
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //フォームから受け取り
+    $user_id = $_POST['user_id'];
+    $password = $_POST['password'];
+
+    //SQL
+    $sql = "SELECT * FROM login
+            WHERE user_id = :user_id
+            AND password = :password";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->bindParam('password', $password);
+
+    $stmt->execute();
+
+    //ユーザーが存在するか
+    if($stmt->rowCount() > 0){
+        //echo "ログイン成功";
+
+        //home画面へ飛ばしたい場合
+        header("Location: ../html/home.html");
+        exit();    
+
+    }
+    else{
+        echo "ユーザーIDまたはパスワードが違います";
+    }   
+}catch(PDOException $e){
+    echo "接続失敗" . $e->getMessage();
+}
+
+
+?>
